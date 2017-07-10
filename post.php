d02ee7bbee5f374e4c23848f29fd06909e843b6b
Munis Isazade <munisisazade@gmail.com>
2017-07-10 13:27:54 +0400

    Update slug

--- post.php
+++ post.php
@@ -30,7 +30,7 @@
 
 
 
-<header class="intro-header" style="background-image: url('<?php echo $_SERVER['SERVER_NAME'] . substr($data['image'],3); ?>')">
+<header class="intro-header" style="background-image: url('<?php echo "http://" . $_SERVER['SERVER_NAME'] . substr($data['image'],3); ?>')">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
