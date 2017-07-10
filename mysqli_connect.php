5a12fd9d750224ed559e42b1d8b0a5534966ad02
Munis Isazade <munisisazade@gmail.com>
2017-07-10 16:37:24 +0400

    Update last php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -65,19 +65,19 @@
         $aditionalflat = "ALTER TABLE  `flatpage` ADD `sub_title` VARCHAR (255) DEFAULT NULL ";
 
         $aditionalflat_two = "ALTER TABLE  `flatpage` ADD `image` VARCHAR (255) DEFAULT NULL ";
-
-        if ($mysqli->query($aditionalflat) === TRUE) {
-            echo "Successfuly update flatpage1 table";
-        }
-        else {
-            echo "ERROR $mysqli->error";
-        }
-        if ($mysqli->query($aditionalflat_two) === TRUE) {
-            echo "Successfuly update flatpage2 table";
-        }
-        else {
-            echo "ERROR $mysqli->error";
-        }
+//
+//        if ($mysqli->query($aditionalflat) === TRUE) {
+//            echo "Successfuly update flatpage1 table";
+//        }
+//        else {
+//            echo "ERROR $mysqli->error";
+//        }
+//        if ($mysqli->query($aditionalflat_two) === TRUE) {
+//            echo "Successfuly update flatpage2 table";
+//        }
+//        else {
+//            echo "ERROR $mysqli->error";
+//        }
 //
 //
 //        if ($mysqli->query($headertable) === TRUE) {
