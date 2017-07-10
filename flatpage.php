5adc6b1957feccaed68779cc320e581f43a18aed
Munis Isazade <munisisazade@gmail.com>
2017-07-10 18:04:38 +0400

    Update last php

--- flatpage.php
+++ flatpage.php
@@ -42,7 +42,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <?php echo $data['text'];?>
+            <?php echo $data['content'];?>
         </div>
     </div>
 </div>
