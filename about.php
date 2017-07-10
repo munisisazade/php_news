1e9a988f545b97338d249c5a7af4c30159ed4460
Munis Isazade <munisisazade@gmail.com>
2017-07-10 18:11:40 +0400

    Update last php

--- about.php
+++ about.php
@@ -11,7 +11,7 @@ function get_data() {
 $data = get_data();
 
 ?>
-<header class="intro-header" style="background-image: url('<?php echo $data['image'];?>')">
+<header class="intro-header" style="background-image: url('<?php echo '/' . $data['image'];?>')">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
