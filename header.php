21a1ec156ffc3a83ae30e2fb1e4dc7ed1241f67c
Munis Isazade <munisisazade@gmail.com>
2017-07-11 17:26:46 +0400

    update

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         var_dump($ini);
         var_dump($file);
         var_dump($_SERVER['DOCUMENT_ROOT']);
-        print_r(parse_ini_file($language));
+        print_r($language);
 
     ?>
     <meta charset="utf-8">
