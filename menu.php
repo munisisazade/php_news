b9dfd32a62832035db6560f6e03bb29db66bc85d
Munis Isazade <munisisazade@gmail.com>
2017-07-11 18:30:27 +0400

    update

--- menu.php
+++ menu.php
@@ -13,6 +13,14 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
+                <li><a href="" data-toggle="dropdown">Dropdown Example
+                        <span class="caret"></span>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">HTML</a></li>
+                            <li><a href="#">CSS</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                        </ul>
+                    </a></li>
                 <li>
                     <a href="/"><?php echo $language->getMenuTitle(); ?></a>
                 </li>
