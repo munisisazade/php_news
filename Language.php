5e7343b52bb32a2280d6687d2ff2cc89149dea6a
Munis Isazade <munisisazade@gmail.com>
2017-07-11 16:24:32 +0400

    update

---
+++ Language.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This is a translation module wich can translate static words
+ * User: munis
+ * Date: 7/11/17
+ * Time: 4:02 PM
+ */
+class Language
+{
+    private $languageArray;
+    private $userLanguage;
+
+    public function __construct($language)
+    {
+        $this->userLanguage = $language;
+        $this->languageArray = self::userLanguage();
+    }
+
+    private function userLanguage()
+    {
+        $file = 'language/config/' . $this->userLanguage . '.ini';
+        if(!file_exists($file))
+        {
+            //Handle Error
+        }
+        return parse_ini_file($file);
+    }
+
+    public function getPageTitle()
+    {
+        return $this->languageArray['PAGE_TITLE'];
+    }
+}
+?>
--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 <head>
     <?php
         global $social_title, $social_desc, $social_img;
+        $lang =  substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        echo $lang;
     ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
