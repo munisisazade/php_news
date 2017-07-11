be3a3f580dc7dcfeaf7f4441f83da97d71d3e8c8
Munis Isazade <munisisazade@gmail.com>
2017-07-11 19:08:07 +0400

    update

--- admin/header_edit.php
+++ admin/header_edit.php
@@ -92,7 +92,7 @@
 
                 <!-- Tab panes -->
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane fade in active" id="profile">asdasd</div>
+                    <div role="tabpanel" class="tab-pane fade in active show" id="profile" aria-expanded="true">asdasd</div>
                     <div role="tabpanel" class="tab-pane fade" id="buzz">bbb</div>
                     <div role="tabpanel" class="tab-pane fade" id="references">ccc</div>
                 </div>
