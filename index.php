101eea590593b43e93f8b958ff69905959550e61
Munis Isazade <munisisazade@gmail.com>
2017-07-28 12:54:20 +0400

    Update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<?php include("header.php"); ?>
 <?php include("menu.php"); ?>
-    <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+    <header class="intro-header" style="background-image: url('/img/home-bg.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
